e5a5e35a58fc1f080eb5c3bbfd744f3ec626c2a3
Taush Sampley <taushsampley@gmail.com>
2024-06-22 14:11:25 -0500

    display concept space screen at ontology root

---
+++ feature/ontology/src/commonMain/kotlin/navigation/OntologyRouting.kt
@@ -0,0 +1,25 @@
+package org.pointyware.commonsense.ontology.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import org.koin.mp.KoinPlatform.getKoin
+import org.pointyware.commonsense.core.navigation.LocationRootScope
+import org.pointyware.commonsense.ontology.ConceptSpaceScreen
+import org.pointyware.commonsense.ontology.viewmodels.ConceptSpaceViewModel
+
+/**
+ *
+ */
+@Composable
+fun LocationRootScope<Any, Any>.ontologyRouting() {
+    location(
+        key = "ontology",
+    ) {
+
+        val koin = remember { getKoin() }
+        val viewModel = remember { koin.get<ConceptSpaceViewModel>() }
+        ConceptSpaceScreen(
+            viewModel = viewModel,
+        )
+    }
+}
