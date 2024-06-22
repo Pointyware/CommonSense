266afe2219555800427ad868c1ecb19f6cf310e4
Taush Sampley <taushsampley@gmail.com>
2024-06-22 14:28:32 -0500

    Bind view model to view in screen composable

---
+++ feature/ontology/src/commonMain/kotlin/ConceptSpaceScreen.kt
@@ -0,0 +1,25 @@
+package org.pointyware.commonsense.ontology
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import org.pointyware.commonsense.ontology.ui.ConceptSpaceUiStateMapper
+import org.pointyware.commonsense.ontology.ui.ConceptSpaceView
+import org.pointyware.commonsense.ontology.viewmodels.ConceptSpaceViewModel
+
+/**
+ * Takes a view model and binds the state and events to the view.
+ */
+@Composable
+fun ConceptSpaceScreen(
+    viewModel: ConceptSpaceViewModel,
+    modifier: Modifier = Modifier,
+) {
+    val state = viewModel.state.collectAsState()
+    ConceptSpaceView(
+        state = ConceptSpaceUiStateMapper.map(state.value),
+        modifier = modifier,
+        onDeleteNode = viewModel::onDeleteNode,
+        onModifyNode = viewModel::onModifyNode
+    )
+}
