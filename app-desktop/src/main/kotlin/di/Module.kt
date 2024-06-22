d6a5d00ad9cb88a218124efe2a7a1f7c69859fa2
Taush Sampley <taushsampley@gmail.com>
2024-06-22 14:43:04 -0500

    Add jvm ontology module to desktop module

--- app-desktop/src/main/kotlin/di/Module.kt
+++ app-desktop/src/main/kotlin/di/Module.kt
@@ -1,6 +1,7 @@
 package org.pointyware.commonsense.desktop.di
 
 import org.koin.dsl.module
+import org.pointyware.commonsense.ontology.local.di.ontologyJvmModule
 import org.pointyware.commonsense.shared.entities.SharedFileResources
 import org.pointyware.commonsense.shared.entities.SharedStringResources
 import org.pointyware.commonsense.shared.ui.SharedDrawableResources
@@ -14,4 +15,6 @@ fun desktopModule() = module {
     single<SharedFontResources> { DesktopFontResources() }
     single<SharedDrawableResources> { DesktopDrawableResources() }
     single<SharedFileResources> { DesktopFileResources() }
+
+    includes(ontologyJvmModule())
 }
