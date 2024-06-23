5ebb2a4ac14251c916ec0f4e3bdaf47d0f553329
Taush Sampley <taushsampley@gmail.com>
2024-06-23 17:05:33 -0500

    Add removal and freeze functions

--- feature/ontology/src/commonMain/kotlin/data/ArrangementController.kt
+++ feature/ontology/src/commonMain/kotlin/data/ArrangementController.kt
@@ -14,11 +14,14 @@ data class Position(
  */
 interface ArrangementController {
     fun addNode(newConcept: Concept, x: Float, y: Float)
+    fun removeNode(id: String)
     fun getConceptPosition(id: String): Position?
 
     fun getConceptPositionOrPut(concept: Concept, x: Float, y: Float): Position {
         return getConceptPosition(concept.id) ?: Position(x, y).also { addNode(concept, x, y)}
     }
+
+    fun freeze(id: String)
 }
 
 
@@ -32,7 +35,15 @@ class SimpleArrangementController(
         concepts[newConcept] = Position(x, y)
     }
 
+    override fun removeNode(id: String) {
+        concepts.remove(concepts.entries.find { it.key.id == id }?.key)
+    }
+
     override fun getConceptPosition(id: String): Position? {
         return concepts.entries.find { it.key.id == id }?.value
+    }
+
+    override fun freeze(id: String) {
+        TODO("Not yet implemented")
     }
 }
