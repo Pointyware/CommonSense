3cc1da6727eafa62bd9c221c0e6c5c996dc3f48a
Taush Sampley <taushsampley@gmail.com>
2024-06-23 17:39:04 -0500

    Add node removal interactor

---
+++ feature/ontology/src/commonMain/kotlin/interactors/RemoveNodeUseCase.kt
@@ -0,0 +1,18 @@
+package org.pointyware.commonsense.ontology.interactors
+
+import org.pointyware.commonsense.ontology.data.ArrangementController
+import org.pointyware.commonsense.ontology.data.ConceptSpaceRepository
+
+/**
+ * Removes a node from the concept space and stops tracking it in the arrangement controller.
+ */
+class RemoveNodeUseCase(
+    private val conceptSpaceRepository: ConceptSpaceRepository,
+    private val arrangementController: ArrangementController
+) {
+    suspend fun invoke(id: String): Result<Unit> {
+        return conceptSpaceRepository.removeNode(id).onSuccess {
+            arrangementController.removeNode(id)
+        }
+    }
+}
