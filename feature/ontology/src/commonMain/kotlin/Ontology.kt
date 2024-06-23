b83cc379e4855f2d3a45d2973db160a819f74285
Taush Sampley <taushsampley@gmail.com>
2024-06-23 17:28:12 -0500

    Create mutable ontology interface and hide implementation behind function

--- feature/ontology/src/commonMain/kotlin/Ontology.kt
+++ feature/ontology/src/commonMain/kotlin/Ontology.kt
@@ -1,5 +1,7 @@
 package org.pointyware.commonsense.ontology
 
+import org.pointyware.commonsense.ontology.local.generateRandomId
+
 /**
  * A collection of concepts and their relations, which usually form a hierarchy of concepts.
  */
@@ -9,10 +11,47 @@ interface Ontology {
     val relations: Set<Relation>
 }
 
-class MutableOntology(
-    override val id: String,
-    override val concepts: MutableSet<Concept> = mutableSetOf(),
-    override val relations: MutableSet<Relation> = mutableSetOf()
-): Ontology {
+interface MutableOntology: Ontology {
+    fun addConcept(concept: Concept)
+    fun addRelation(conceptSource: String, conceptTarget: String)
+}
 
+internal class SimpleMutableOntology(
+    override val id: String,
+): MutableOntology {
+
+    private val conceptMap: MutableMap<String, Concept> = mutableMapOf()
+    override val concepts: Set<Concept> get() = conceptMap.values.toSet()
+
+    private val relationMap: MutableMap<String, Relation> = mutableMapOf()
+    override val relations: Set<Relation> get() = relationMap.values.toSet()
+
+    override fun addConcept(concept: Concept) {
+        conceptMap[concept.id]?.let { tenant ->
+            throw IllegalArgumentException("Concept ${tenant.id} already exists in Ontology $id")
+        } ?: run {
+            conceptMap[concept.id] = concept
+        }
+    }
+
+    override fun addRelation(conceptSource: String, conceptTarget: String) {
+        conceptMap[conceptSource]?.let { source ->
+            conceptMap[conceptTarget]?.let { target ->
+                val newRelation = MemberRelation(
+                    id = generateRandomId(),
+                    owner = this
+                )
+                relationMap[newRelation.id] = newRelation
+            } ?: throw IllegalArgumentException("Concept target $conceptTarget not found in Ontology $id")
+        } ?: throw IllegalArgumentException("Concept source $conceptSource not found in Ontology $id")
+    }
+}
+
+/**
+ * Create a new mutable ontology with the given [id] and apply the [init] block to it.
+ */
+fun mutableOntology(id: String, init: MutableOntology.() -> Unit = {}): MutableOntology {
+    val ontology = SimpleMutableOntology(id)
+    ontology.init()
+    return ontology
 }
