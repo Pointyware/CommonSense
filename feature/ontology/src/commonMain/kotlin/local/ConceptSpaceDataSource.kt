07cad26c65f180c5c74579c8835b3557c9d7758b
Taush Sampley <taushsampley@gmail.com>
2024-06-23 16:32:42 -0500

    Make createNode suspending

--- feature/ontology/src/commonMain/kotlin/local/ConceptSpaceDataSource.kt
+++ feature/ontology/src/commonMain/kotlin/local/ConceptSpaceDataSource.kt
@@ -9,7 +9,7 @@ import org.pointyware.commonsense.ontology.ConceptSpace
 interface ConceptSpaceDataSource {
     suspend fun loadConceptSpace(id: String): Result<ConceptSpace>
     suspend fun saveConceptSpace(space: ConceptSpace): Result<Unit>
-    fun createNode(name: String): Result<Concept>
+    suspend fun createNode(name: String): Result<Concept>
 }
 
 class ConceptSpaceNotFoundException(id: String) : Exception("Concept space with id $id not found")
