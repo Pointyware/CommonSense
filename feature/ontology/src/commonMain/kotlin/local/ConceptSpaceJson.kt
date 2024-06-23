870a966781d600cf5c91f4f064d557b22447edc4
Taush Sampley <taushsampley@gmail.com>
2024-06-23 17:27:50 -0500

    Add supporting json DTOs

--- feature/ontology/src/commonMain/kotlin/local/ConceptSpaceJson.kt
+++ feature/ontology/src/commonMain/kotlin/local/ConceptSpaceJson.kt
@@ -1,8 +1,47 @@
 package org.pointyware.commonsense.ontology.local
 
-/**
- *
- */
-class ConceptSpaceJson {
+import kotlinx.serialization.Serializable
+import org.pointyware.commonsense.ontology.RelationWeight
 
-}
+/**
+ * A JSON representation of the [org.pointyware.commonsense.ontology.ConceptSpace] entity.
+ */
+@Serializable
+data class ConceptSpaceJson(
+    val id: String,
+    val focus: OntologyJson
+)
+
+
+/**
+ * A JSON representation of the [org.pointyware.commonsense.ontology.Ontology] entity.
+ */
+@Serializable
+data class OntologyJson(
+    val id: String,
+    val concepts: Set<ConceptJson>,
+    val relations: Set<RelationJson>
+)
+
+/**
+ * A JSON representation of the [org.pointyware.commonsense.ontology.Concept] entity.
+ */
+@Serializable
+data class ConceptJson(
+    val id: String,
+    val name: String,
+    val description: String?,
+    val relations: Set<String>
+)
+
+/**
+ * A JSON representation of the [org.pointyware.commonsense.ontology.Relation] entity.
+ */
+@Serializable
+data class RelationJson(
+    val id: String,
+    val name: String,
+    val source: String,
+    val target: String,
+    val weight: RelationWeight,
+)
