564720afeb31e0b317b966532a7b213b91c8a597
Taush Sampley <taushsampley@gmail.com>
2024-06-24 20:07:05 -0500

    Add KmpFile

---
+++ core/local/src/jvmSharedMain/kotlin/KmpFile.jvmShared.kt
@@ -0,0 +1,11 @@
+import org.pointyware.commonsense.core.local.KmpFile
+
+class JvmFile(
+    override val path: String
+): KmpFile {
+
+}
+
+actual fun file(path: String): KmpFile {
+    return JvmFile(path)
+}
