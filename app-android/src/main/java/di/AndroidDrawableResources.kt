892eed23f185a9bb9ffca8815a3277cb724e20ee
Taush Sampley <taushsampley@gmail.com>
2024-06-25 13:06:59 -0500

    Add missing android resources class

---
+++ app-android/src/main/java/di/AndroidDrawableResources.kt
@@ -0,0 +1,12 @@
+package org.pointyware.commonsense.android.di
+
+import org.jetbrains.compose.resources.DrawableResource
+import org.pointyware.commonsense.shared.ui.SharedDrawableResources
+
+/**
+ * Binds the string resources for the Android platform.
+ */
+class AndroidDrawableResources: SharedDrawableResources {
+    override val appIconSmall: DrawableResource
+        get() = TODO("Not yet implemented")
+}
