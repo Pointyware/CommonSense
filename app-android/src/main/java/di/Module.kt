075008ee93d424bf406bc2f69f61b977fc32bdf0
Taush Sampley <taushsampley@gmail.com>
2024-06-25 13:26:10 -0500

    satisfy android resource dependencies

--- app-android/src/main/java/di/Module.kt
+++ app-android/src/main/java/di/Module.kt
@@ -1,11 +1,17 @@
 package org.pointyware.commonsense.android.di
 
 import org.koin.dsl.module
+import org.pointyware.commonsense.shared.entities.SharedFileResources
 import org.pointyware.commonsense.shared.entities.SharedStringResources
+import org.pointyware.commonsense.shared.ui.SharedDrawableResources
+import org.pointyware.commonsense.shared.ui.SharedFontResources
 
 /**
  *
  */
 fun androidModule() = module {
     single<SharedStringResources> { AndroidStringResources() }
+    single<SharedFontResources> { AndroidFontResources() }
+    single<SharedDrawableResources> { AndroidDrawableResources() }
+    single<SharedFileResources> { AndroidFileResources() }
 }
