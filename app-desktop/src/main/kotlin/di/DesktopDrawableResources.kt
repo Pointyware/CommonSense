a02859d85cba162caabeb9b111fd4a08a47705e3
Taush Sampley <taushsampley@gmail.com>
2024-06-25 12:57:13 -0500

    move tray icon to shared drawable resources for injection

--- app-desktop/src/main/kotlin/di/DesktopDrawableResources.kt
+++ app-desktop/src/main/kotlin/di/DesktopDrawableResources.kt
@@ -1,7 +1,11 @@
 package org.pointyware.commonsense.desktop.di
 
+import org.jetbrains.compose.resources.DrawableResource
+import org.pointyware.commonsense.desktop.Res
+import org.pointyware.commonsense.desktop.tray_icon
 import org.pointyware.commonsense.shared.ui.SharedDrawableResources
 
 class DesktopDrawableResources : SharedDrawableResources {
-
+    override val appIconSmall: DrawableResource
+        get() = Res.drawable.tray_icon
 }
--- app-shared/src/commonMain/kotlin/ui/SharedDrawableResources.kt
+++ app-shared/src/commonMain/kotlin/ui/SharedDrawableResources.kt
@@ -1,7 +1,10 @@
 package org.pointyware.commonsense.shared.ui
 
+import org.jetbrains.compose.resources.DrawableResource
+
 /**
  * Defines drawable resources shared by all platforms.
  */
 interface SharedDrawableResources {
+    val appIconSmall: DrawableResource
 }
