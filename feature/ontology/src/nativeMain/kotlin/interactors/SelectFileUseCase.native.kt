3fd26337bb9fab7189f4eeccb8fd818fb0bcfb60
Taush Sampley <taushsampley@gmail.com>
2024-06-24 20:56:54 -0500

    correct native interactor signature

--- feature/ontology/src/nativeMain/kotlin/interactors/SelectFileUseCase.native.kt
+++ feature/ontology/src/nativeMain/kotlin/interactors/SelectFileUseCase.native.kt
@@ -1,11 +1,12 @@
 package org.pointyware.commonsense.ontology.interactors
 
+import org.pointyware.commonsense.core.local.KmpFile
+
 /**
  *
  */
 actual class SelectFileUseCase actual constructor() {
-    actual suspend fun invoke(): Result<String> {
+    actual suspend operator fun invoke(): Result<KmpFile> {
         TODO("Not yet implemented")
     }
-
 }
