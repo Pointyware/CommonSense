20d0648e8f8e47e1e8aa473fdfb1666afb651035
Taush Sampley <taushsampley@gmail.com>
2024-06-25 13:50:34 -0500

    add todo note

--- feature/ontology/src/jvmMain/kotlin/interactors/SelectFileUseCase.jvm.kt
+++ feature/ontology/src/jvmMain/kotlin/interactors/SelectFileUseCase.jvm.kt
@@ -8,6 +8,12 @@ import org.pointyware.commonsense.core.local.KmpFile
  */
 actual class SelectFileUseCase actual constructor() {
     actual suspend operator fun invoke(): Result<KmpFile> {
+        /*
+        TODO: Implement file selection for JVM. This will likely involve using a file dialog
+          1. Instead of expect/actual define cross-platform interface for file selection
+          2. Implement in higher layer for access to platform-specific file dialog
+          3. Bind implementations for each platform
+         */
         return Result.success(JvmFile(DIRECTORY_SPACES)) // TODO: coordinate with file dialogs
     }
 }
