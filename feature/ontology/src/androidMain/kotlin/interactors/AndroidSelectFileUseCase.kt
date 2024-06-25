e0b9ce95f4da15d879a597f86f1df574fa1596c4
Taush Sampley <taushsampley@gmail.com>
2024-06-25 18:50:19 -0500

    Split expect/actual into platform definitions

--- feature/ontology/src/androidMain/kotlin/interactors/AndroidSelectFileUseCase.kt
+++ feature/ontology/src/androidMain/kotlin/interactors/AndroidSelectFileUseCase.kt
@@ -5,9 +5,8 @@ import org.pointyware.commonsense.core.local.KmpFile
 /**
  *
  */
-actual class SelectFileUseCase actual constructor() {
-    actual suspend operator fun invoke(): Result<KmpFile> {
+class AndroidSelectFileUseCase(): SelectFileUseCase {
+    override suspend operator fun invoke(): Result<KmpFile> {
         TODO("Not yet implemented")
     }
-
 }
--- feature/ontology/src/commonMain/kotlin/interactors/SelectFileUseCase.kt
+++ feature/ontology/src/commonMain/kotlin/interactors/SelectFileUseCase.kt
@@ -3,9 +3,10 @@ package org.pointyware.commonsense.ontology.interactors
 import org.pointyware.commonsense.core.local.KmpFile
 
 const val DIRECTORY_SPACES = "spaces"
+
 /**
- *
+ * Opens a file dialog to select a file and returns the result.
  */
-expect class SelectFileUseCase() {
+interface SelectFileUseCase {
     suspend operator fun invoke(): Result<KmpFile>
 }
