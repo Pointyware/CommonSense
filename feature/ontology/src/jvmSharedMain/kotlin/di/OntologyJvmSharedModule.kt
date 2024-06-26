2f80965d187c9d93023d3f06f15cc063af3e69e9
Taush Sampley <taushsampley@gmail.com>
2024-06-25 19:14:03 -0500

    Set :feature:ontology module info

--- feature/ontology/src/jvmSharedMain/kotlin/di/OntologyJvmSharedModule.kt
+++ feature/ontology/src/jvmSharedMain/kotlin/di/OntologyJvmSharedModule.kt
@@ -3,7 +3,7 @@ package org.pointyware.commonsense.ontology.di
 import kotlinx.serialization.json.Json
 import org.koin.dsl.module
 import org.pointyware.commonsense.ontology.local.ConceptSpaceDataSource
-import org.pointyware.commonsense.ontology.local.ConceptSpaceJsonDataSource
+import org.pointyware.commonsense.feature.ontology.local.ConceptSpaceJsonDataSource
 
 /**
  * Provides JVM-specific ontology dependencies
