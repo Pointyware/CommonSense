641cba88af5a0b49ce0dc4bbbc450c382409f1fa
Taush Sampley <taushsampley@gmail.com>
2024-06-25 19:05:24 -0500

    remove todo note

--- feature/ontology/src/jvmMain/kotlin/interactors/JvmSelectFileUseCase.kt
+++ feature/ontology/src/jvmMain/kotlin/interactors/JvmSelectFileUseCase.kt
@@ -7,14 +7,11 @@ import org.pointyware.commonsense.core.local.KmpFile
  *
  */
 class JvmSelectFileUseCase(
-
+    /*
+    TODO: coordinate with file dialogs
+     */
 ): SelectFileUseCase {
     override suspend operator fun invoke(): Result<KmpFile> {
-        /*
-        TODO: Implement file selection for JVM. This will likely involve using a file dialog
-          2. Implement in higher layer for access to platform-specific file dialog
-          3. Bind implementations for each platform
-         */
         return Result.success(JvmFile(DIRECTORY_SPACES)) // TODO: coordinate with file dialogs
     }
 }
