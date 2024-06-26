8b3320b7a154daedb16e145d0fab3021e5feb8c4
Taush Sampley <taushsampley@gmail.com>
2024-06-26 11:31:27 -0500

    remove doc

--- core/local/src/commonMain/kotlin/LocalStorage.kt
+++ core/local/src/commonMain/kotlin/LocalStorage.kt
@@ -2,16 +2,6 @@ package org.pointyware.commonsense.core.local
 
 /**
  * Multiplatform local data storage. Usually implemented as a file on most targets, but for web it could be localStorage.
- *
- * ```kotlin
- * class LocalStorage(path) {
- *
- * }
- *
- * expect object LocalStorageConstants {
- *   val maxBytes: Long
- * }
- * ```
  */
 interface LocalStorage {
     val path: String
