d787a7c77ff5d712710ca209539a703d5cbd0541
Taush Sampley <taushsampley@gmail.com>
2024-06-26 11:58:25 -0500

    Allow deletion and modification of concept nodes

--- core/ui/src/commonMain/kotlin/components/InfoNode.kt
+++ core/ui/src/commonMain/kotlin/components/InfoNode.kt
@@ -2,23 +2,32 @@ package org.pointyware.commonsense.core.ui.components
 
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import org.pointyware.commonsense.core.common.Log
+import org.pointyware.commonsense.core.common.Uuid
 
 /**
  *
  */
-data class InfoNodeState( // TODO: add modification state
-    val id: String,
+data class InfoNodeState(
+    val id: Uuid,
     val title: String,
     val x: Dp,
-    val y: Dp
+    val y: Dp,
+    val modificationEnabled: Boolean = false,
 ) {
 }
 
@@ -29,9 +38,10 @@ data class InfoNodeState( // TODO: add modification state
 fun InfoNode(
     state: InfoNodeState,
     modifier: Modifier = Modifier,
-    onModify: (String) -> Unit,
-    onDelete: (String) -> Unit,
-) { // TODO: add modification/deletion buttons and reflect modification state with TextInput
+    onModify: (Uuid) -> Unit,
+    onDelete: (Uuid) -> Unit,
+    onComplete: (String) -> Unit,
+) {
     Log.v("InfoNode")
     Box(
         modifier = modifier
@@ -41,6 +51,27 @@ fun InfoNode(
                 shape = RoundedCornerShape(4.dp)
             ),
     ) {
-        Text(text = state.title)
+        if (state.modificationEnabled) {
+            var modifiedValue by remember { mutableStateOf(state.title) }
+            Column() {
+                TextField(
+                    value = state.title,
+                    onValueChange = { it -> modifiedValue = it}
+                )
+                Button(onClick = { onComplete(modifiedValue) }) {
+                    Text(text = "Done")
+                }
+            }
+        } else {
+            Column() {
+                Text(text = state.title)
+                Button(onClick = { onModify(state.id) }) {
+                    Text(text = "Modify")
+                }
+                Button(onClick = { onDelete(state.id) }) {
+                    Text(text = "Delete")
+                }
+            }
+        }
     }
 }
