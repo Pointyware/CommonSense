c3dded5d2508eb46b4c9084cf0ca35148c70c5b3
Taush Sampley <taushsampley@gmail.com>
2024-06-26 13:06:11 -0500

    Add edge label to edge info state

--- core/ui/src/commonMain/kotlin/components/InfoEdge.kt
+++ core/ui/src/commonMain/kotlin/components/InfoEdge.kt
@@ -1,10 +1,15 @@
 package org.pointyware.commonsense.core.ui.components
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
 
 
 data class InfoEdgeState(
     val id: String,
+    val label: String?,
     val from: InfoNodeState,
     val to: InfoNodeState
 )
@@ -15,9 +20,20 @@ data class InfoEdgeState(
 @Composable
 fun InfoEdge(
     state: InfoEdgeState,
+    modifier: Modifier = Modifier,
     onModify: () -> Unit,
     onDelete: () -> Unit,
     onComplete: () -> Unit,
 ) {
-    // TODO: Draw elements of edge without worrying about placement; edge layout will take care of it
+    Box(
+        modifier = modifier
+            .drawBehind {
+                // TODO: draw edge
+            }
+    ) {
+        state.label?.let { label ->
+            Text(text = label)
+        }
+        // TODO: add edge controls
+    }
 }
