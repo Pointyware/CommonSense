fcac34f0b921dceff97fa1c56eedc158cc1a2f9b
Taush Sampley <taushsampley@gmail.com>
2024-06-27 17:36:10 -0500

    Add copied plugin

---
+++ build-logic/conventions/src/main/kotlin/org/pointyware/painteddogs/buildlogic/ComposeTargetsConventionPlugin.kt
@@ -0,0 +1,58 @@
+package org.pointyware.commonsense.buildlogic
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.dependencies
+import org.jetbrains.kotlin.gradle.dsl.kotlinExtension
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
+import org.jetbrains.kotlin.gradle.plugin.mpp.pm20.util.targets
+
+/**
+ * A plugin that sets up the KMP targets convention for the project.
+ */
+class ComposeTargetsConventionPlugin: Plugin<Project> {
+
+    override fun apply(target: Project) {
+//        target.plugins.apply("koin")
+        target.plugins.apply("org.jetbrains.compose")
+//        target.plugins.apply("koin")
+        target.plugins.apply("org.jetbrains.kotlin.plugin.compose")
+
+        // Configure Kotlin targets
+        target.kotlinExtension.targets.forEach {
+            when (it) {
+                is KotlinNativeTarget -> {
+                    it.binaries.framework {
+                        baseName = "app_shared"
+                        isStatic = true
+                    }
+                }
+                else -> {
+                    it.compilations.all {
+                        kotlinOptions {
+                            apiVersion = "1.8"
+                        }
+                    }
+                }
+            }
+        }
+
+        // Add dependencies after evaluation
+        target.afterEvaluate {
+
+            val coreProject = project(":core:entities")
+            val ontologyProject = project(":feature:ontology")
+            val epistemologyProject = project(":feature:epistemology")
+
+            dependencies {
+                listOf(
+                    coreProject,
+                    ontologyProject,
+                    epistemologyProject
+                ).forEach {
+                    add("implementation", it)
+                }
+            }
+        }
+    }
+}
