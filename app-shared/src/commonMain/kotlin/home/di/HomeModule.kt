5d7d923c41189477bc2f1a44aa41e2d3271b93ed
Taush Sampley <taushsampley@gmail.com>
2024-06-27 15:32:32 -0500

    Move Home Dependency Injection files for consistency

--- app-shared/src/commonMain/kotlin/home/di/HomeModule.kt
+++ app-shared/src/commonMain/kotlin/home/di/HomeModule.kt
@@ -1,4 +1,4 @@
-package org.pointyware.commonsense.shared.di
+package org.pointyware.commonsense.shared.home.di
 
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
@@ -6,7 +6,6 @@ import org.pointyware.commonsense.feature.ontology.navigation.ontologyRoute
 import org.pointyware.commonsense.shared.home.HomeUiStateMapper
 import org.pointyware.commonsense.shared.home.HomeViewModel
 import org.pointyware.commonsense.shared.home.HomeViewModelImpl
-import org.pointyware.commonsense.shared.home.homeRoute
 
 /**
  * Defines productions bindings to satisfy interface requests.
