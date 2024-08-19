caadebe9f9cf64c14af9d2805097bfee9118b4e3
Taush Sampley <taushsampley@gmail.com>
2024-08-19 17:28:16 -0500

    Add ktor convention plugins

---
+++ build-logic/conventions/src/main/kotlin/KtorServerConventionPlugin.kt
@@ -0,0 +1,17 @@
+package org.pointyware.commonsense.buildlogic
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.TaskAction
+
+/**
+ * A plugin that sets up ktor dependencies for server modules.
+ */
+class KtorServerConventionPlugin: Plugin<Project> {
+
+    override fun apply(target: Project) {
+
+    }
+}
