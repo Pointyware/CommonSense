41cbfb9f1e606217d2120c578708a9c57372b4e1
Taush Sampley <taushsampley@gmail.com>
2024-08-30 13:16:27 -0500

    Create `Category` entity

---
+++ feature/ontology/src/commonMain/kotlin/entities/Category.kt
@@ -0,0 +1,11 @@
+package org.pointyware.commonsense.feature.ontology.entities
+
+import org.pointyware.commonsense.core.common.Uuid
+
+/**
+ * Represents a category in the ontology.
+ */
+class Category(
+    val id: Uuid,
+    val name: String
+)
