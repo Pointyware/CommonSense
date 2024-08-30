119a10d8d4f629c8e4ddab01b2946d6eb98a8e99
Taush Sampley <taushsampley@gmail.com>
2024-08-30 15:28:35 -0500

    Add CategoryCreator composable

---
+++ feature/ontology/src/commonMain/kotlin/ui/CategoryCreator.kt
@@ -0,0 +1,41 @@
+package org.pointyware.commonsense.feature.ontology.ui
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import org.pointyware.commonsense.feature.ontology.viewmodels.ConceptEditorUiState
+
+/**
+ * Allows a user to edit the properties of a new or existing concept.
+ */
+@Composable
+fun CategoryCreator(
+    state: ConceptEditorUiState,
+    modifier: Modifier = Modifier,
+    onNameChange: (String) -> Unit,
+    onCancel: () -> Unit,
+    onConfirm: () -> Unit,
+) {
+    Column(
+        modifier = modifier
+    ) {
+        TextField(
+            label = { Text("Name") },
+            value = state.name,
+            onValueChange = onNameChange,
+        )
+
+        Row {
+            Button(onClick = onCancel) {
+                Text("Cancel")
+            }
+            Button(onClick = onConfirm) {
+                Text("Confirm")
+            }
+        }
+    }
+}
