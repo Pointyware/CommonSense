8742d67528dadcc4da81bd04c65f7d439401f760
Taush Sampley <taushsampley@gmail.com>
2024-09-27 13:44:33 -0500

    Add ConceptEditorUiState doc

--- feature/ontology/src/commonMain/kotlin/viewmodels/ConceptEditorUiState.kt
+++ feature/ontology/src/commonMain/kotlin/viewmodels/ConceptEditorUiState.kt
@@ -3,7 +3,7 @@ package org.pointyware.commonsense.feature.ontology.viewmodels
 import org.pointyware.commonsense.core.common.Uuid
 
 /**
- *
+ * Represents the state of a Concept Editor UI.
  */
 data class ConceptEditorUiState(
     val id: Uuid?,
