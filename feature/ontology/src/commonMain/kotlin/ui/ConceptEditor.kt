d49b0745c331673c675f3c17f2b70da599861168
Taush Sampley <taushsampley@gmail.com>
2024-09-27 16:59:22 -0500

    Add drop down button template selector

--- feature/ontology/src/commonMain/kotlin/ui/ConceptEditor.kt
+++ feature/ontology/src/commonMain/kotlin/ui/ConceptEditor.kt
@@ -1,11 +1,17 @@
 package org.pointyware.commonsense.feature.ontology.ui
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import org.pointyware.commonsense.feature.ontology.viewmodels.ConceptEditorUiState
 
@@ -24,6 +30,22 @@ fun ConceptEditor(
     Column(
         modifier = modifier
     ) {
+        Box {
+            var expanded by remember { mutableStateOf(false) }
+            Button(onClick = { expanded = true }) {
+                Text("Select Template")
+            }
+            DropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false }
+            ) {
+                Button(
+                    onClick = { expanded = false }
+                ) {
+                    Text("Concept")
+                }
+            }
+        }
         TextField(
             label = { Text("Name") },
             value = state.name,
