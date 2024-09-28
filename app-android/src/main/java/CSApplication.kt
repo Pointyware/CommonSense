f9c3af8337540ced84ff03a73ff6794cbc897a3e
Taush Sampley <taushsampley@gmail.com>
2024-09-28 00:11:20 -0500

    replace explicit startKoin with setupKoin function

--- app-android/src/main/java/CSApplication.kt
+++ app-android/src/main/java/CSApplication.kt
@@ -1,9 +1,9 @@
 package org.pointyware.commonsense.android
 
 import android.app.Application
-import org.koin.core.context.startKoin
+import org.koin.dsl.module
 import org.pointyware.commonsense.android.di.androidModule
-import org.pointyware.commonsense.shared.di.appModule
+import org.pointyware.commonsense.shared.di.setupKoin
 
 /**
  * This is the production Common Sense application; it performs production environment setup.
@@ -12,11 +12,10 @@ class CSApplication: Application() {
     override fun onCreate() {
         super.onCreate()
 
-        startKoin {
-            modules(
-                androidModule(),
-                appModule()
-            )
-        }
+        setupKoin(
+            platformModule = module {
+                includes(androidModule(this@CSApplication))
+            }
+        )
     }
 }
--- app-android/src/main/java/di/Module.kt
+++ app-android/src/main/java/di/Module.kt
@@ -1,5 +1,7 @@
 package org.pointyware.commonsense.android.di
 
+import android.app.Application
+import android.content.Context
 import org.koin.dsl.module
 import org.pointyware.commonsense.shared.entities.SharedFileResources
 import org.pointyware.commonsense.shared.entities.SharedStringResources
@@ -9,9 +11,12 @@ import org.pointyware.commonsense.shared.ui.SharedFontResources
 /**
  *
  */
-fun androidModule() = module {
+fun androidModule(application: Application) = module {
     single<SharedStringResources> { AndroidStringResources() }
     single<SharedFontResources> { AndroidFontResources() }
     single<SharedDrawableResources> { AndroidDrawableResources() }
     single<SharedFileResources> { AndroidFileResources() }
+
+    single<Application> { application }
+    single<Context> { application }
 }
