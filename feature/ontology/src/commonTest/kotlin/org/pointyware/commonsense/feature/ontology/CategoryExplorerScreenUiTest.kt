ff72e45364f5c09d92aaecfb1a95d751e4c13373
Taush Sampley <taushsampley@gmail.com>
2024-09-28 15:15:57 -0500

    Create negative test cases

--- feature/ontology/src/commonTest/kotlin/org/pointyware/commonsense/feature/ontology/CategoryExplorerScreenUiTest.kt
+++ feature/ontology/src/commonTest/kotlin/org/pointyware/commonsense/feature/ontology/CategoryExplorerScreenUiTest.kt
@@ -107,8 +107,65 @@ class CategoryExplorerScreenUiTest {
         // TODO: add test for new concept presence
     }
 
+    /**
+     * User Journey: Create New Concept and Cancel
+     */
     @Test
-    fun tapping_add_category_should_display_category_editor() = runComposeUiTest {
+    fun tapping_add_concept_should_display_concept_editor_and_cancel() = runComposeUiTest {
+            /*
+            Given:
+            - A concept space
+             */
+            contentUnderTest()
+
+            /*
+            When:
+            - The user taps "New Concept"
+            Then:
+            - The concept editor is displayed
+            - And the name and description fields are empty
+            - And the "Save" button is disabled
+             */
+            onNodeWithText("New Concept").performClick()
+
+            waitUntilExactlyOneExists(hasContentDescription("Concept Editor"))
+            onNodeWithText("Name")
+                .assertEditableTextEquals("")
+            onNodeWithText("Description")
+                .assertEditableTextEquals("")
+            onNodeWithText("Save")
+                .assertIsNotEnabled()
+
+            /*
+            When:
+            - The user enters a name and description
+            Then:
+            - The "Save" button is enabled
+             */
+            onNodeWithText("Name")
+                .performTextInput("Concept Name")
+            onNodeWithText("Description")
+                .performTextInput("Concept Description")
+            onNodeWithText("Save")
+                .assertIsEnabled()
+
+            /*
+            When:
+            - The user taps "Cancel"
+            Then:
+            - The concept editor is dismissed
+             */
+            onNodeWithText("Cancel").performClick()
+            waitUntilDoesNotExist(hasContentDescription("Concept Editor"))
+
+            // TODO: add test for new concept absence
+        }
+
+    /**
+     * User Journey: Create New Category and Confirm
+     */
+    @Test
+    fun tapping_add_category_should_display_category_editor_and_confirm() = runComposeUiTest {
         /*
         Given:
         - A concept space
@@ -148,5 +205,50 @@ class CategoryExplorerScreenUiTest {
         waitUntilDoesNotExist(hasContentDescription("Category Editor"))
 
         // TODO: add test for new category presence
+    }
+
+    /**
+     * User Journey: Create New Category and Cancel
+     */
+    @Test
+    fun tapping_add_category_should_display_category_editor_and_cancel() = runComposeUiTest {
+        /*
+        Given:
+        - A concept space
+         */
+        contentUnderTest()
+
+        /*
+        When:
+        - The user taps "New Category"
+        Then:
+        - The category editor is displayed
+        - And the name field is empty
+        - And the "Save" button is disabled
+         */
+        onNodeWithText("New Category").performClick()
+
+        waitUntilExactlyOneExists(hasContentDescription("Category Editor"))
+
+        /*
+        When:
+        - The user enters a name
+        Then:
+        - The "Save" button is enabled
+         */
+        onNodeWithText("Name")
+            .performTextInput("Category Name")
+        onNodeWithText("Save")
+            .assertIsEnabled()
+
+        /*
+        When:
+        - The user taps "Cancel"
+        Then:
+        - The category editor is dismissed
+         */
+        onNodeWithText("Cancel").performClick()
+        waitUntilDoesNotExist(hasContentDescription("Category Editor"))
+        // TODO: add test for new category absence
     }
 }
