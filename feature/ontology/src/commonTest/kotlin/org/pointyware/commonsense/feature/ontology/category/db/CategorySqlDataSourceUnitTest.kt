7170ba9632606400817721b184e52ffc48e89f4f
Taush Sampley <taushsampley@gmail.com>
2024-09-29 19:43:09 -0500

    Add category sql data source unit test

---
+++ feature/ontology/src/commonTest/kotlin/org/pointyware/commonsense/feature/ontology/category/db/CategorySqlDataSourceUnitTest.kt
@@ -0,0 +1,62 @@
+package org.pointyware.commonsense.feature.ontology.category.db
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import org.koin.core.context.stopKoin
+import org.koin.mp.KoinPlatform.getKoin
+import org.pointyware.commonsense.core.common.Uuid
+import org.pointyware.commonsense.feature.ontology.category.data.CategorySqlDataSource
+import org.pointyware.commonsense.feature.ontology.test.setupKoin
+import kotlin.test.assertTrue
+import kotlin.test.AfterTest
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+import kotlin.test.assertNotEquals
+import kotlin.test.assertNotNull
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class CategorySqlDataSourceUnitTest {
+
+    private lateinit var dataSource: CategorySqlDataSource
+
+    @BeforeTest
+    fun setUp() {
+        setupKoin()
+        val testDispatcher = StandardTestDispatcher()
+        Dispatchers.setMain(testDispatcher)
+        val koin = getKoin()
+        dataSource = koin.get()
+    }
+
+    @AfterTest
+    fun tearDown() {
+        stopKoin()
+        Dispatchers.resetMain()
+    }
+
+    @Test
+    fun insertCategoryAtRoot() = runTest {
+        /*
+        Given:
+        - A category with the name "test"
+         */
+        val categoryName = "test"
+
+        /*
+        When:
+        - The category is inserted into the database
+        Then:
+        - The result is success
+        - The uuid of the category is not null
+         */
+        val result = dataSource.createCategory("test")
+
+        assertTrue(result.isSuccess)
+        assertNotNull(result.getOrNull()?.id)
+        assertNotEquals(Uuid.nil(), result.getOrThrow().id)
+    }
+}
