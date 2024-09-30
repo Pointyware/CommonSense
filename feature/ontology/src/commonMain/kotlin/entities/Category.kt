9b56139c6e2dbcae30f2bfc2e42ce2af1876cf91
Taush Sampley <taushsampley@gmail.com>
2024-09-29 20:33:19 -0500

    Make category data class

--- feature/ontology/src/commonMain/kotlin/entities/Category.kt
+++ feature/ontology/src/commonMain/kotlin/entities/Category.kt
@@ -5,7 +5,7 @@ import org.pointyware.commonsense.core.common.Uuid
 /**
  * Represents a category in the ontology.
  */
-class Category(
+data class Category(
     val id: Uuid,
     val name: String
 )
