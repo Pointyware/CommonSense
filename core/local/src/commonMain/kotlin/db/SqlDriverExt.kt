cfd8e9ceb96562c63200d42af55ecd0f999dacc0
Taush Sampley <taushsampley@gmail.com>
2024-10-02 01:11:31 -0500

    Add extension function to create new db or migrate existing db

--- core/local/src/commonMain/kotlin/db/SqlDriverExt.kt
+++ core/local/src/commonMain/kotlin/db/SqlDriverExt.kt
@@ -2,6 +2,7 @@ package org.pointyware.commonsense.core.local.db
 
 import app.cash.sqldelight.db.QueryResult
 import app.cash.sqldelight.db.SqlDriver
+import app.cash.sqldelight.db.SqlSchema
 
 /**
  * Extension property to get and set the version of the database.
@@ -23,3 +24,17 @@ var SqlDriver.version: Long
             binders = null
         )
     }
+
+/**
+ * Extension function to create or migrate the database schema.
+ */
+fun <T : QueryResult<Unit>> SqlSchema<T>.createOrMigrate(driver: SqlDriver) {
+    val lastVersion = driver.version
+    val newVersion = version
+    if (lastVersion == 0L) {
+        create(driver)
+        driver.version = newVersion
+    } else {
+        migrate(driver, lastVersion, newVersion)
+    }
+}
