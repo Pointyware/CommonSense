78a240779e4655afefc3f5bcae80e88105c5a5b4
Taush Sampley <taushsampley@gmail.com>
2024-10-02 20:25:11 -0500

    Add long press interaction helper function

--- feature/ontology/src/commonTest/kotlin/org/pointyware/commonsense/feature/ontology/test/SemanticsNodeInteractionExt.kt
+++ feature/ontology/src/commonTest/kotlin/org/pointyware/commonsense/feature/ontology/test/SemanticsNodeInteractionExt.kt
@@ -1,10 +1,13 @@
 package org.pointyware.commonsense.feature.ontology.test
 
 import androidx.compose.ui.semantics.SemanticsProperties
+import androidx.compose.ui.test.MainTestClock
 import androidx.compose.ui.test.SemanticsMatcher.Companion.expectValue
 import androidx.compose.ui.test.SemanticsNodeInteraction
 import androidx.compose.ui.test.assert
+import androidx.compose.ui.test.performTouchInput
 import androidx.compose.ui.text.AnnotatedString
+import kotlinx.datetime.Clock
 
 /**
  *
@@ -12,3 +15,10 @@ import androidx.compose.ui.text.AnnotatedString
 fun SemanticsNodeInteraction.assertEditableTextEquals(expected: String) {
     assert(expectValue(SemanticsProperties.EditableText, AnnotatedString(expected)))
 }
+
+fun SemanticsNodeInteraction.performLongPress(mainClock: MainTestClock) =
+    performTouchInput {
+        down(center)
+        mainClock.advanceTimeBy(500)
+        up()
+    }
