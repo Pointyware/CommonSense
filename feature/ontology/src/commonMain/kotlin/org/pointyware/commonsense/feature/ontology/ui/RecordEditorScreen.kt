187d1bde7b5739894592094ffef9750bd892e214
Taush Sampley <taushsampley@gmail.com>
2024-10-07 16:48:21 -0500

    Replace ui todo with view model mapping

--- feature/ontology/src/commonMain/kotlin/org/pointyware/commonsense/feature/ontology/ui/RecordEditorScreen.kt
+++ feature/ontology/src/commonMain/kotlin/org/pointyware/commonsense/feature/ontology/ui/RecordEditorScreen.kt
@@ -1,23 +1,30 @@
 package org.pointyware.commonsense.feature.ontology.ui
 
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import org.pointyware.commonsense.feature.ontology.viewmodels.RecordEditorUiState
+import org.pointyware.commonsense.feature.ontology.viewmodels.RecordEditorViewModel
 
 /**
  * Allows a user to create a new type of record.
  */
 @Composable
 fun RecordEditorScreen(
-    state: RecordEditorUiState,
-    modifier: Modifier = Modifier,
+    viewModel: RecordEditorViewModel
 ) {
-    /*
-    TODO: implement UI
-      1. Create a text field for the class name
-      2. Create a button to add a new field - allow user to add new field
-      3. Create a list of fields
-        1. create name input for field name
-        2. create type dropdown for field type
-     */
+    val state by viewModel.state.collectAsState()
+    RecordEditor(
+        state = state,
+        modifier = Modifier.fillMaxSize(),
+        onNameChange = viewModel::onRecordNameChange,
+        onFieldAdded = viewModel::onFieldAdded,
+        onFieldNameChange = viewModel::onFieldNameChange,
+        onFieldTypeChanged = viewModel::onFieldTypeChange,
+        onFieldValueChanged = viewModel::onFieldValueChange,
+        onFieldRemoved = viewModel::onFieldRemoved,
+        onConfirm = viewModel::onConfirm,
+        onCancel = viewModel::onCancel,
+    )
 }
