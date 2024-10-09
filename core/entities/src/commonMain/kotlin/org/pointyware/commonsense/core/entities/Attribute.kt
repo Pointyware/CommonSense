325d63c142ee8eae72913ff20e3341bacdd0df9b
Taush Sampley <taushsampley@gmail.com>
2024-10-09 12:46:55 -0500

    Add attribute class to mirror field definitions

---
+++ core/entities/src/commonMain/kotlin/org/pointyware/commonsense/core/entities/Attribute.kt
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2024 Pointyware. Use of this software is governed by the GPL-3.0 license.
+ */
+
+package org.pointyware.commonsense.core.entities
+
+/**
+ * An [Attribute] is a part of a [Value.Instance] that defines a [Field] of a [Record].
+ */
+data class Attribute<T:Type>(
+    val name: String,
+    val value: Value<T>
+)
