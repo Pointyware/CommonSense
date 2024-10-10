0d3806b5647fd8e191127460aa0964eda23c40ae
Taush Sampley <taushsampley@gmail.com>
2024-10-10 12:58:27 -0500

    Replace orphaned uuids with instance holders

--- feature/ontology/src/commonMain/kotlin/org/pointyware/commonsense/feature/ontology/data/RecordsDataSource.kt
+++ feature/ontology/src/commonMain/kotlin/org/pointyware/commonsense/feature/ontology/data/RecordsDataSource.kt
@@ -4,15 +4,17 @@
 
 package org.pointyware.commonsense.feature.ontology.data
 
-import org.pointyware.commonsense.core.common.Uuid
 import org.pointyware.commonsense.core.entities.Type
 import org.pointyware.commonsense.core.entities.Value
+import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
 
+@OptIn(ExperimentalUuidApi::class)
 interface RecordsDataSource {
     suspend fun createRecord(name: String): Result<Type.Record>
-    suspend fun <T:Type> addField(recordId: Uuid, name: String, type: T, defaultValue: Value<T>): Result<Type.Record>
+    suspend fun <T:Type> addField(original: Type.Record, name: String, type: T, defaultValue: Value<T>): Result<Type.Record>
     suspend fun getRecord(id: Uuid): Result<Type.Record>
 
-    suspend fun createInstance(recordId: Uuid, name: String): Result<Value.Instance>
-    suspend fun <T:Type> setAttribute(instanceId: Uuid, fieldId: Uuid, value: Value<T>): Result<Value.Instance>
+    suspend fun createInstance(template: Type.Record): Result<Value.Instance>
+    suspend fun <T:Type> setAttribute(original: Value.Instance, fieldName: String, value: Value<T>): Result<Value.Instance>
 }
