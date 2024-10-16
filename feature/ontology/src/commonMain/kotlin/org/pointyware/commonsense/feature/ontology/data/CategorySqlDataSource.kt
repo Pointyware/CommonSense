8c7a90aec587ea47170acba5db949a996de4913b
Taush Sampley <taushsampley@gmail.com>
2024-10-15 19:22:23 -0500

    Implement stubs with kotlin uuid

--- feature/ontology/src/commonMain/kotlin/org/pointyware/commonsense/feature/ontology/data/CategorySqlDataSource.kt
+++ feature/ontology/src/commonMain/kotlin/org/pointyware/commonsense/feature/ontology/data/CategorySqlDataSource.kt
@@ -1,18 +1,21 @@
 package org.pointyware.commonsense.feature.ontology.data
 
-import org.pointyware.commonsense.core.common.Uuid
 import org.pointyware.commonsense.core.local.db.createOrMigrate
+import org.pointyware.commonsense.feature.ontology.Category
 import org.pointyware.commonsense.feature.ontology.Concept
 import org.pointyware.commonsense.feature.ontology.IndependentConcept
 import org.pointyware.commonsense.feature.ontology.db.OntologyDb
-import org.pointyware.commonsense.feature.ontology.Category
 import org.pointyware.commonsense.feature.ontology.local.DriverFactory
 import org.pointyware.commonsense.feature.ontology.local.Persistence
+import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
+import org.pointyware.commonsense.core.common.Uuid as MyUuid
 
 
 /**
  *
  */
+@OptIn(ExperimentalUuidApi::class)
 @Deprecated("Use RecordsSqlDataSource")
 class CategorySqlDataSource(
     private val driverFactory: DriverFactory,
@@ -26,27 +29,54 @@ class CategorySqlDataSource(
     }
 
     override suspend fun createCategory(name: String): Result<Category> = runCatching {
-        val newUuid = Uuid.v4()
-        db.categoryQueries.insertCategory(newUuid.bytes, ByteArray(1), name)
+        val newUuid = Uuid.random()
+        db.categoryQueries.insertCategory(newUuid.toByteArray(), ByteArray(1), name)
+        Category(newUuid, name)
+    }
+
+    override suspend fun addCategory(subject: MyUuid, name: String): Result<Category> = runCatching {
+        val newUuid = Uuid.random()
+        db.categoryQueries.insertCategory(newUuid.toByteArray(), subject.bytes, name)
         Category(newUuid, name)
     }
 
     override suspend fun addCategory(subject: Uuid, name: String): Result<Category> = runCatching {
-        val newUuid = Uuid.v4()
-        db.categoryQueries.insertCategory(newUuid.bytes, subject.bytes, name)
+        val newUuid = Uuid.random()
+        db.categoryQueries.insertCategory(newUuid.toByteArray(), subject.toByteArray(), name)
         Category(newUuid, name)
     }
 
-    override suspend fun getCategory(id: Uuid): Result<Category> = runCatching {
+    override suspend fun getCategory(id: MyUuid): Result<Category> = runCatching {
         db.categoryQueries.getCategory(id.bytes) { uuid, name ->
-            Category(Uuid(uuid), name)
+            Category(Uuid.fromByteArray(uuid), name)
+        }.executeAsOne()
+    }
+    override suspend fun getCategory(id: Uuid): Result<Category> = runCatching {
+        db.categoryQueries.getCategory(id.toByteArray()) { uuid, name ->
+            Category(Uuid.fromByteArray(uuid), name)
         }.executeAsOne()
     }
 
-    override suspend fun getSubcategories(id: Uuid): Result<List<Category>> = runCatching {
+    override suspend fun getSubcategories(id: MyUuid): Result<List<Category>> = runCatching {
         db.categoryQueries.getCategories(id.bytes) { uuid, name ->
-            Category(Uuid(uuid), name)
+            Category(Uuid.fromByteArray(uuid), name)
         }.executeAsList()
+    }
+
+    override suspend fun getSubcategories(id: Uuid): Result<List<Category>> = runCatching {
+        db.categoryQueries.getCategories(id.toByteArray()) { uuid, name ->
+            Category(Uuid.fromByteArray(uuid), name)
+        }.executeAsList()
+    }
+
+    override suspend fun addConcept(
+        subject: MyUuid,
+        name: String,
+        description: String
+    ): Result<Concept> = runCatching {
+        val newUuid = Uuid.random()
+        db.categoryQueries.insertConcept(subject.bytes, newUuid.toByteArray(), name, description)
+        IndependentConcept(newUuid, name, description)
     }
 
     override suspend fun addConcept(
@@ -54,22 +84,36 @@ class CategorySqlDataSource(
         name: String,
         description: String
     ): Result<Concept> = runCatching {
-        val newUuid = Uuid.v4()
-        db.categoryQueries.insertConcept(subject.bytes, newUuid.bytes, name, description)
+        val newUuid = Uuid.random()
+        db.categoryQueries.insertConcept(subject.toByteArray(), newUuid.toByteArray(), name, description)
         IndependentConcept(newUuid, name, description)
     }
 
-    override suspend fun getConcepts(id: Uuid): Result<List<Concept>> = runCatching {
+    override suspend fun getConcepts(id: MyUuid): Result<List<Concept>> = runCatching {
         db.categoryQueries.getConcepts(id.bytes) { uuid, name, description ->
-            IndependentConcept(Uuid(uuid), name, description)
+            IndependentConcept(Uuid.fromByteArray(uuid), name, description)
         }.executeAsList()
     }
 
-    override suspend fun removeCategories(ids: Set<Uuid>): Result<Unit> = runCatching {
+    override suspend fun getConcepts(id: Uuid): Result<List<Concept>> = runCatching {
+        db.categoryQueries.getConcepts(id.toByteArray()) { uuid, name, description ->
+            IndependentConcept(Uuid.fromByteArray(uuid), name, description)
+        }.executeAsList()
+    }
+
+    override suspend fun removeCategories(ids: Set<MyUuid>): Result<Unit> = runCatching {
         db.categoryQueries.deleteCategories(ids.map { it.bytes })
     }
 
-    override suspend fun removeConcepts(ids: Set<Uuid>): Result<Unit> = runCatching{
+    override suspend fun removeCategories(ids: Set<Uuid>): Result<Unit> = runCatching {
+        db.categoryQueries.deleteCategories(ids.map { it.toByteArray() })
+    }
+
+    override suspend fun removeConcepts(ids: Set<MyUuid>): Result<Unit> = runCatching{
         db.categoryQueries.deleteConcepts(ids.map { it.bytes })
+    }
+
+    override suspend fun removeConcepts(ids: Set<Uuid>): Result<Unit> = runCatching {
+        db.categoryQueries.deleteConcepts(ids.map { it.toByteArray() })
     }
 }
