8f3e95af16c7f8f17f673bc7dc3404701e5e7134
Taush Sampley <taushsampley@gmail.com>
2024-10-29 13:46:05 -0500

    Create queries unit test to verify query behavior

---
+++ feature/ontology/src/commonTest/kotlin/org/pointyware/commonsense/feature/ontology/data/db/RecordsQueriesUnitTest.kt
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2024 Pointyware. Use of this software is governed by the GPL-3.0 license.
+ */
+
+package org.pointyware.commonsense.feature.ontology.data.db
+
+import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
+import org.koin.mp.KoinPlatform.getKoin
+import org.pointyware.commonsense.core.local.db.createOrMigrate
+import org.pointyware.commonsense.feature.ontology.db.OntologyDb
+import org.pointyware.commonsense.feature.ontology.db.RecordsQueries
+import org.pointyware.commonsense.feature.ontology.di.ontologyLocalPlatformModule
+import org.pointyware.commonsense.feature.ontology.local.DriverFactory
+import org.pointyware.commonsense.feature.ontology.local.Persistence
+import kotlin.test.AfterTest
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
+
+/**
+ *
+ */
+@OptIn(ExperimentalUuidApi::class)
+class RecordsQueriesUnitTest {
+
+    private lateinit var unitUnderTest: RecordsQueries
+
+    @BeforeTest
+    fun setUp() {
+        startKoin {
+            modules(ontologyLocalPlatformModule())
+        }
+        val koin = getKoin()
+        val driverFactory = koin.get<DriverFactory>()
+        val driver = driverFactory.createSqlDriver(Persistence.InMemory)
+        OntologyDb.Schema.createOrMigrate(driver)
+        unitUnderTest = RecordsQueries(driver)
+    }
+
+    @AfterTest
+    fun tearDown() {
+        stopKoin()
+    }
+
+    @Test
+    fun getIntField_should_return_default_for_null_instance() {
+        val defaultValue = 42L
+        val recordId = Uuid.random().toByteArray()
+        val nullId = Uuid.NIL.toByteArray()
+        unitUnderTest.createRecord(recordId, "foobar")
+        unitUnderTest.addIntField(recordId, "field", 1)
+        unitUnderTest.createInstance(recordId, nullId)
+        unitUnderTest.setInstanceIntValue(nullId, recordId, "field", defaultValue)
+
+        val result = unitUnderTest.getIntField(nullId, recordId, "field").executeAsOneOrNull()
+
+        assertEquals(defaultValue, result, "Int Field should have $defaultValue default value")
+    }
+}
