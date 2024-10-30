42448a9ee112dcaafb0541ca0415730fb2025099
Taush Sampley <taushsampley@gmail.com>
2024-10-30 16:47:12 -0500

    replace category repository with records repository and *concept* with *instance* references

--- feature/ontology/src/commonMain/kotlin/org/pointyware/commonsense/feature/ontology/data/RecordsRepository.kt
+++ feature/ontology/src/commonMain/kotlin/org/pointyware/commonsense/feature/ontology/data/RecordsRepository.kt
@@ -5,11 +5,20 @@
 package org.pointyware.commonsense.feature.ontology.data
 
 import org.pointyware.commonsense.core.entities.Type
+import org.pointyware.commonsense.core.entities.Value
+import org.pointyware.commonsense.feature.ontology.Category
+import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
 
 /**
  *
  */
+@OptIn(ExperimentalUuidApi::class)
 interface RecordsRepository {
     suspend fun createRecord(name: String): Result<Type.Record>
-
+    suspend fun addCategory(subject: Uuid, newCategory: Category): Result<Category>
+    suspend fun getCategory(categoryId: Uuid): Result<Category>
+    suspend fun getSubcategories(id: Uuid): Result<List<Category>>
+    suspend fun addInstance(subject: Uuid, newInstance: Value.Instance): Result<Value.Instance>
+    suspend fun getInstances(id: Uuid): Result<List<Value.Instance>>
 }
--- feature/ontology/src/commonMain/kotlin/org/pointyware/commonsense/feature/ontology/interactors/CreateNewInstanceUseCase.kt
+++ feature/ontology/src/commonMain/kotlin/org/pointyware/commonsense/feature/ontology/interactors/CreateNewInstanceUseCase.kt
@@ -1,24 +1,21 @@
 package org.pointyware.commonsense.feature.ontology.interactors
 
-import org.pointyware.commonsense.feature.ontology.data.CategoryRepository
+import org.pointyware.commonsense.core.entities.Value
 import org.pointyware.commonsense.feature.ontology.data.ConceptEditorController
-import org.pointyware.commonsense.feature.ontology.Category
 import org.pointyware.commonsense.feature.ontology.data.RecordsRepository
-import org.pointyware.commonsense.feature.ontology.local.generateRandomId
 import kotlin.uuid.ExperimentalUuidApi
 
 /**
  * Creates a new concept in the currently selected category.
  */
 @OptIn(ExperimentalUuidApi::class)
-class CreateNewCategoryUseCase(
+class CreateNewInstanceUseCase(
     private val conceptEditorController: ConceptEditorController,
     private val recordsRepository: RecordsRepository
 ) {
-    suspend fun invoke(name: String): Result<Category> {
+    suspend fun invoke(newInstance: Value.Instance): Result<Value.Instance> {
         val subject = conceptEditorController.subject?.id
             ?: return Result.failure(IllegalStateException("No subject selected"))
-        val newCategory = Category(generateRandomId(), name)
-        return recordsRepository.addCategory(subject, newCategory)
+        return recordsRepository.addInstance(subject, newInstance)
     }
 }
