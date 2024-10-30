c4755f34667a09ea1fbbab7a9ca243f905444082
Taush Sampley <taushsampley@gmail.com>
2024-10-30 11:24:56 -0500

    Add ui state docs

--- feature/ontology/src/commonMain/kotlin/org/pointyware/commonsense/feature/ontology/viewmodels/CategoryEditorUiState.kt
+++ feature/ontology/src/commonMain/kotlin/org/pointyware/commonsense/feature/ontology/viewmodels/CategoryEditorUiState.kt
@@ -5,11 +5,17 @@ import kotlin.uuid.Uuid
 
 
 /**
- *
+ * Represents the state of a [Category] being created and/or modified.
  */
 @OptIn(ExperimentalUuidApi::class)
 data class CategoryEditorUiState(
+    /**
+     * The id of the category being edited. If null, a new category is being created.
+     */
     val id: Uuid?,
+    /**
+     * The name of the category.
+     */
     val name: String,
 ) {
     companion object {
