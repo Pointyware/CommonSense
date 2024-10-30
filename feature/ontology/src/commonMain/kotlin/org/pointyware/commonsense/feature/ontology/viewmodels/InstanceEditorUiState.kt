53eb8398d0cdb081dcf8cab509bcf8b6537ec5d5
Taush Sampley <taushsampley@gmail.com>
2024-10-30 11:43:43 -0500

    Add uuids to instance/record editors

--- feature/ontology/src/commonMain/kotlin/org/pointyware/commonsense/feature/ontology/viewmodels/InstanceEditorUiState.kt
+++ feature/ontology/src/commonMain/kotlin/org/pointyware/commonsense/feature/ontology/viewmodels/InstanceEditorUiState.kt
@@ -5,11 +5,16 @@
 package org.pointyware.commonsense.feature.ontology.viewmodels
 
 import org.pointyware.commonsense.core.entities.Type
+import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
 
 /**
  * Represents the state of an [Instance] being created or modified.
  */
+@OptIn(ExperimentalUuidApi::class)
 data class InstanceEditorUiState(
+    val id: Uuid?,
     val type: Type.Record,
     val fields: List<FieldEditorUiState<Type>>,
-)
+) {
+}
