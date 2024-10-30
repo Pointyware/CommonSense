0235aae075e41f899721355aeead022669ddab02
Taush Sampley <taushsampley@gmail.com>
2024-10-30 11:46:33 -0500

    remove concept editor route

--- feature/ontology/src/commonMain/kotlin/org/pointyware/commonsense/feature/ontology/navigation/OntologyRouting.kt
+++ feature/ontology/src/commonMain/kotlin/org/pointyware/commonsense/feature/ontology/navigation/OntologyRouting.kt
@@ -11,18 +11,15 @@ import org.pointyware.commonsense.core.common.Log
 import org.pointyware.commonsense.core.navigation.LocationRootScope
 import org.pointyware.commonsense.core.navigation.StaticRoute
 import org.pointyware.commonsense.feature.ontology.ConceptSpaceScreen
+import org.pointyware.commonsense.feature.ontology.ui.CategoryEditor
 import org.pointyware.commonsense.feature.ontology.ui.CategoryExplorerScreen
 import org.pointyware.commonsense.feature.ontology.viewmodels.CategoryEditorViewModel
 import org.pointyware.commonsense.feature.ontology.viewmodels.CategoryExplorerViewModel
-import org.pointyware.commonsense.feature.ontology.ui.CategoryEditor
-import org.pointyware.commonsense.feature.ontology.ui.ConceptEditor
-import org.pointyware.commonsense.feature.ontology.viewmodels.ConceptEditorViewModel
 import org.pointyware.commonsense.feature.ontology.viewmodels.ConceptSpaceViewModel
 
 val ontologyRoute = StaticRoute("ontology", Unit)
 val categoryExplorer = ontologyRoute.fixed("categoryExplorer")
 val categoryCreator = ontologyRoute.fixed("categoryCreator")
-val conceptEditor = ontologyRoute.fixed("conceptEditor")
 
 /**
  *
@@ -66,25 +63,6 @@ fun LocationRootScope<Any, Any>.ontologyRouting() {
             state = state,
             modifier = Modifier.fillMaxSize(),
             onNameChange = viewModel::onNameChange,
-            onCancel = viewModel::onCancel,
-            onConfirm = viewModel::onConfirm,
-        )
-    }
-
-    location(
-        key = conceptEditor
-    ) {
-
-        Log.v("ConceptEditor")
-        val koin = remember { getKoin() }
-        val viewModel = remember { koin.get<ConceptEditorViewModel>() }
-
-        val state by viewModel.state.collectAsState()
-        ConceptEditor(
-            state = state,
-            modifier = Modifier.fillMaxSize(),
-            onNameChange = viewModel::onNameChange,
-            onDescriptionChange = viewModel::onDescriptionChange,
             onCancel = viewModel::onCancel,
             onConfirm = viewModel::onConfirm,
         )
