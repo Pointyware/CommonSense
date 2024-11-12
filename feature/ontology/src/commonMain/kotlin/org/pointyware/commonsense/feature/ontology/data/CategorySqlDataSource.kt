b16836f1aaaae6f8673445b4fd6a1da0721ae824
Taush Sampley <taushsampley@gmail.com>
2024-11-12 12:31:20 -0600

    Provide stubbed implementation of category data source

---
+++ feature/ontology/src/commonMain/kotlin/org/pointyware/commonsense/feature/ontology/data/CategorySqlDataSource.kt
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2024 Pointyware. Use of this software is governed by the GPL-3.0 license.
+ */
+
+package org.pointyware.commonsense.feature.ontology.data
+
+import org.pointyware.commonsense.feature.ontology.Category
+import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
+
+@OptIn(ExperimentalUuidApi::class)
+class CategorySqlDataSource : CategoryDataSource {
+    override suspend fun createCategory(name: String): Result<Category> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun addCategory(subject: Uuid, name: String): Result<Category> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getCategory(id: Uuid): Result<Category> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getSubcategories(id: Uuid): Result<List<Category>> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun removeCategories(ids: Set<Uuid>): Result<Unit> {
+        TODO("Not yet implemented")
+    }
+}
