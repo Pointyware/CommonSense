228a52b14dd12d8aa4db3681f80a9227a523ffed
Taush Sampley <taushsampley@gmail.com>
2024-11-23 19:03:22 -0600

    Migrate db creation and dependencies into koin module

--- feature/ontology/src/commonMain/kotlin/org/pointyware/commonsense/feature/ontology/di/OntologyModule.kt
+++ feature/ontology/src/commonMain/kotlin/org/pointyware/commonsense/feature/ontology/di/OntologyModule.kt
@@ -1,5 +1,6 @@
 package org.pointyware.commonsense.feature.ontology.di
 
+import app.cash.sqldelight.db.SqlDriver
 import kotlinx.serialization.json.Json
 import org.koin.core.module.Module
 import org.koin.core.module.dsl.bind
@@ -7,6 +8,7 @@ import org.koin.core.module.dsl.binds
 import org.koin.core.module.dsl.factoryOf
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
+import org.pointyware.commonsense.core.local.db.createOrMigrate
 import org.pointyware.commonsense.feature.ontology.data.ArrangementController
 import org.pointyware.commonsense.feature.ontology.data.CategoryDataSource
 import org.pointyware.commonsense.feature.ontology.data.CategorySqlDataSource
@@ -19,6 +21,7 @@ import org.pointyware.commonsense.feature.ontology.data.RecordsRepository
 import org.pointyware.commonsense.feature.ontology.data.RecordsRepositoryImpl
 import org.pointyware.commonsense.feature.ontology.data.RecordsSqlDataSource
 import org.pointyware.commonsense.feature.ontology.data.SimpleArrangementController
+import org.pointyware.commonsense.feature.ontology.db.OntologyDb
 import org.pointyware.commonsense.feature.ontology.interactors.AddNewNodeUseCase
 import org.pointyware.commonsense.feature.ontology.interactors.CreateNewCategoryUseCase
 import org.pointyware.commonsense.feature.ontology.interactors.CreateNewInstanceUseCase
@@ -32,6 +35,8 @@ import org.pointyware.commonsense.feature.ontology.interactors.SelectFileUseCase
 import org.pointyware.commonsense.feature.ontology.interactors.SelectFileUseCaseImpl
 import org.pointyware.commonsense.feature.ontology.interactors.UpdateNodeUseCase
 import org.pointyware.commonsense.feature.ontology.local.ConceptSpaceDataSource
+import org.pointyware.commonsense.feature.ontology.local.DriverFactory
+import org.pointyware.commonsense.feature.ontology.local.Persistence
 import org.pointyware.commonsense.feature.ontology.space.ui.ConceptSpaceUiStateMapper
 import org.pointyware.commonsense.feature.ontology.space.viewmodels.ConceptSpaceViewModel
 import org.pointyware.commonsense.feature.ontology.viewmodels.CategoryEditorViewModel
@@ -71,6 +76,16 @@ expect fun ontologyLocalPlatformModule(): Module
 fun ontologyLocalModule() = module {
     single<RecordsDataSource> { RecordsSqlDataSource(get())}
     singleOf(::CategorySqlDataSource) { bind<CategoryDataSource>() }
+    single<Lazy<OntologyDb>> { lazy {
+        val driver: SqlDriver = get()
+        OntologyDb.Schema.createOrMigrate(driver)
+        OntologyDb(driver)
+    } }
+    single<SqlDriver> {
+        val persistence: Persistence? = getOrNull()
+        val driverFactory = get<DriverFactory>()
+        driverFactory.createSqlDriver(persistence ?: Persistence.File)
+    }
 
     includes(
         ontologyLocalPlatformModule()
